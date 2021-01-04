73d52f523dae1bea9bb21bbaaaef440c6d483b52
Mallorax <patryk733773@gmail.com>
2021-01-04 21:32:36 +0100

    added player character, controls and basic animations

---
+++ Unity_project/Assets/Scripts/Player/GroundCheck.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour
+{
+    //prepared for ground colission logic
+    public bool isGrounded;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        isGrounded = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isGrounded = false;
+    } 
+
+}
---
+++ Unity_project/Assets/Scripts/Player/PlayerControler.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public Animator anim;
+    private enum State {idle, run, jump, rest};
+    private State state = State.idle;
+    private Collider2D coll;
+    [SerializeField] private LayerMask ground;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        coll = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hDirection = Input.GetAxis("Horizontal");
+        //Movement
+        if (hDirection < 0)
+        {
+            rb.velocity = new Vector2(-10, rb.velocity.y);
+            Vector2 tmp = transform.localScale;
+            tmp.x = 1;
+            transform.localScale = tmp;
+        }
+        else if (hDirection > 0)
+        {
+            rb.velocity = new Vector2(10, rb.velocity.y);
+            Vector2 tmp = transform.localScale;
+            tmp.x = -1;
+            transform.localScale = tmp;
+        }
+        else
+        {
+        }
+        if (Input.GetButtonDown("Jump") && coll.IsTouchingLayers(ground))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 30);
+            state = State.jump;
+        }
+        VelocityStateChange();
+        anim.SetInteger("state", (int)state);
+    }
+
+    private void VelocityStateChange()
+    {
+        if(state == State.jump)
+        {
+            if (coll.IsTouchingLayers(ground))
+            {
+                state = State.idle;
+            }
+        }else if(Mathf.Abs(rb.velocity.x) > 1)
+        {
+            //running
+            state = State.run;
+
+        }else{
+            state = State.idle;
+        }
+    }
+
+}
