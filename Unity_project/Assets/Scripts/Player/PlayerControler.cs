4b7aad53b519e56c4ea1e799bb5811d6781a8940
Mallorax <patryk733773@gmail.com>
2021-01-07 00:50:30 +0100

    Final touches to animations

--- Unity_project/Assets/Scripts/Player/PlayerControler.cs
+++ Unity_project/Assets/Scripts/Player/PlayerControler.cs
@@ -1,15 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerControler : MonoBehaviour
 {
     public Rigidbody2D rb;
     public Animator anim;
-    private enum State {idle, run, jump, rest};
-    private State state = State.idle;
     private Collider2D coll;
-    [SerializeField] private LayerMask ground;
+
+    //States
+    private enum State { idle, run, jump, rest };
+    private State state = State.idle;
+
+    //Inspector Variables
+    [SerializeField]
+    private LayerMask ground;
+    [SerializeField]
+    private float jumpForce;
+    [SerializeField]
+    private float characterSpeed;
+    [SerializeField]
+    private int hp;
+    [SerializeField]
+    private Text playerHPText;
+
+    private int direction;
 
 
     // Start is called before the first frame update
@@ -21,50 +37,74 @@ public class PlayerControler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        playerHPText.text = hp.ToString();
+        CharacterMovement();
+        VelocityStateChange();
+        anim.SetInteger("state", (int)state);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            if (state == State.jump)
+            {
+                Destroy(collision.gameObject);
+            }
+            hp--;
+        }
+    }
+
+    private void CharacterMovement()
+    {
         float hDirection = Input.GetAxis("Horizontal");
         //Movement
         if (hDirection < 0)
         {
-            rb.velocity = new Vector2(-10, rb.velocity.y);
+            direction = -1;
+            rb.velocity = new Vector2(-characterSpeed, rb.velocity.y);
             Vector2 tmp = transform.localScale;
             tmp.x = 1;
             transform.localScale = tmp;
         }
         else if (hDirection > 0)
         {
-            rb.velocity = new Vector2(10, rb.velocity.y);
+            direction = 1;
+            rb.velocity = new Vector2(characterSpeed, rb.velocity.y);
             Vector2 tmp = transform.localScale;
             tmp.x = -1;
             transform.localScale = tmp;
         }
-        else
-        {
-        }
         if (Input.GetButtonDown("Jump") && coll.IsTouchingLayers(ground))
         {
-            rb.velocity = new Vector2(rb.velocity.x, 30);
-            state = State.jump;
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
-        VelocityStateChange();
-        anim.SetInteger("state", (int)state);
     }
 
     private void VelocityStateChange()
     {
-        if(state == State.jump)
-        {
-            if (coll.IsTouchingLayers(ground))
-            {
-                state = State.idle;
-            }
-        }else if(Mathf.Abs(rb.velocity.x) > 1)
-        {
-            //running
-            state = State.run;
 
-        }else{
+        if (Input.GetButtonDown("Fire1"))
+        {
+            anim.SetTrigger("attack");
+        }
+        else if (Input.GetButtonDown("Fire3"))
+        {
+            anim.SetTrigger("rest");
+        }
+        else if (!coll.IsTouchingLayers(ground))
+        {
+            state = State.jump;
+        }
+        else if (Mathf.Abs(rb.velocity.x) > 1 && coll.IsTouchingLayers(ground))
+        {
+            state = State.run;
+        }
+        else if (Mathf.Abs(rb.velocity.x) < 1 && coll.IsTouchingLayers(ground))
+        {
             state = State.idle;
         }
+
     }
 
 }
