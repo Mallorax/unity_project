97904e7fdf5825b56f314f749dd1f2dfa700a1c5
DESKTOP-Malloraz <patryk733773@gmail.com>
2021-01-20 22:30:53 +0100

    Added boss track

---
+++ Unity_project/Assets/PlayBossTrack.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayBossTrack : MonoBehaviour
+{
+    private AudioSource audio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Music on!! " + gameObject.tag);
+        if (gameObject.CompareTag("MusicTriger"))
+        {            
+            audio.Play();
+            GetComponent<BoxCollider2D>().enabled = false;
+
+        }
+    }
+}
